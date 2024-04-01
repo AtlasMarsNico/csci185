02cccb4248ac870750a8603fc4e0cbf6304f8df8
Atlas <nicole122204@outlook.com>
2024-04-01 11:05:59 -0400

    szdfgjhjhgfdfvbn

--- tutorials/tutorial08/main.js
+++ tutorials/tutorial08/main.js
@@ -14,29 +14,44 @@ function mouseDragged(){
     // the color, shape, and size of the paintbrush that are selected
     // in the right-hand panel. Replace the code below with something
     // smarter:
-    fill('hotpink');
-    circle(mouseX, mouseY, 20);
+
+    const color= document.querySelector("#color").value;
+    const size= Number(document.querySelector("#size").value);
+    const shape= document.querySelector("#shape").value;
+
+    fill(color);
+    strokeWeight(0);
+
+    if (shape === "circle") {
+        circle(mouseX, mouseY, size);
+    } else if (shape === "square") {
+        square(mouseX, mouseY, size);
+    } else {
+        triangle (
+            mouseX, mouseY,
+            mouseX-size/2, mouseY+size,
+            mouseX+size/2, mouseY+size
+        );
+    }
 }
 
 
 
 
- Samples:
+//  https://p5js.org/reference/#/p5/circle
+// fill('pink');
+// circle(400, 400, 100);   // x, y, diameter
 
- https://p5js.org/reference/#/p5/circle
-fill('pink');
-circle(400, 400, 100);   // x, y, diameter
+//  https://p5js.org/reference/#/p5/square
+// fill('navy');
+// square(50, 300, 50);   // x (top left), y (top left), width
 
- https://p5js.org/reference/#/p5/square
-fill('navy');
-square(50, 300, 50);   // x (top left), y (top left), width
-
- https://p5js.org/reference/#/p5/triangle
-fill('teal');
-triangle(
-    50, 50, // first point
-    30, 80, // second point
-    70, 80  // third point
-)
+//  https://p5js.org/reference/#/p5/triangle
+// fill('teal');
+// triangle(
+//     50, 50, // first point
+//     30, 80, // second point
+//     70, 80  // third point
+// )
 
 
