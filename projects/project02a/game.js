0c424b6ca556d74439ebe74739d125c6abcf05a1
Atlas <nicole122204@outlook.com>
2024-04-27 21:18:35 -0400

    plz help

--- projects/project02a/game.js
+++ projects/project02a/game.js
@@ -3,58 +3,34 @@ let y = 200;
 let width = 100;
 let color = "red";
 
-
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
     drawPlayer();
 }
 
-
 function drawPlayer() {
-    clear();
+    // clear();
     fill(color);
     circle(x, y, width); // x position, y position, diameter
     drawGrid(window.innerWidth, window.innerHeight);
 }
-
 function movePlayer(ev) {
-    // open the console in your browser to see what happens :)
     console.log(ev.code);
-    if (ev.code === "ArrowUp") {
-        y = y - 5;
+    if (ev.code == "ArrowUp") {
+        console.log("up arrow!");
+        y -= 4;
+    } else if (ev.code == "ArrowDown") {
+        console.log("down arrow!");
+        y += 4;
+    } else if (ev.code == "ArrowLeft") {
+        console.log("left arrow!");
+        x -= 4;
+    } else if (ev.code == "ArrowRight") {
+        console.log("right arrow!");
+        x += 4;
+    } else if (ev.code == "Space") {
+        width += 4;
+    } else if (ev.code == "Escape") {
+        width -= 4;
     }
-
-    else if (ev.code === "ArrowDown") {
-        y = y + 5;
-    }
-
-    else if (ev.code === "ArrowLeft") {
-        x = x - 5;
-    }
-
-    else if (ev.code === "ArrowRight") {
-        x = x + 5;
-    }
-
-    else if (ev.code === "Space") {
-        width = width + 5;
-    }
-
-    else if (ev.code === "Escape") {
-        width = width - 5;
-    }
-
-    else if (ev.code === "KeyB") {
-        color = "blue";
-    }
-
-    else if (ev.code === "KeyR") {
-        color = "red";
-    }
-
-    else if (ev.code === "KeyT") {
-        color = "teal";
-    }
-    // re-draw the player
-    drawPlayer();
 }
